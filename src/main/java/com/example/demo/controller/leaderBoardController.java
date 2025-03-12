c3128431cde4a1d4a5ab8e609a41a4bbfd3f4ac9
Klézli Richárd <h265202@stud.u-szeged.hu>
2025-03-12 20:53:59 +0100

    feat: Added a button to take player back from leaderboard to Menu

--- src/main/java/com/example/demo/controller/leaderBoardController.java
+++ src/main/java/com/example/demo/controller/leaderBoardController.java
@@ -1,8 +1,15 @@
 package com.example.demo.controller;
 
+import com.example.demo.HelloApplication;
 import com.example.demo.model.Leaderboard;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class leaderBoardController {
     @FXML
@@ -11,5 +18,13 @@ public class leaderBoardController {
 
     public TableView<Leaderboard> getTableView() {
         return tableView;
+    }
+
+    public void goBack(ActionEvent actionEvent) throws IOException {
+        FXMLLoader MenuLoader = new FXMLLoader(HelloApplication.class.getResource("Menu.fxml"));
+        Scene scene = new Scene(MenuLoader.load(), 480, 600);
+        Stage stage = (Stage) tableView.getScene().getWindow();
+        stage.setTitle("MineSweeperFX!");
+        stage.setScene(scene);
     }
 }
